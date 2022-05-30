122b4ca51625df06e472a8485d5bcc91a964cb27
Julian <julianmork@gmail.com>
2022-05-30 19:13:05 +0200

    Glium Shader setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 extern crate glium;
+
+use std::time::Instant;
+use glium::{implement_vertex, uniform};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+
 fn main() {
     use glium::glutin;
     use glium::Surface;
@@ -12,9 +24,47 @@ fn main() {
 
 
 
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+     #version 140
+
+    in vec2 position;
+
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+"#;
+
+    let fragment_shader_src = r#"
+     #version 140
+    uniform float time;
+    out vec4 color;
+
+    void main() {
+        color = vec4(1.0, cos(time), 0.0, 1.0);
+    }
+"#;
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let start = Instant::now();
+
+
     event_loop.run(move |ev, _, control_flow| {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+        let tim = start.elapsed().as_secs_f32();
+        let uniforms = uniform! {
+            time: tim
+        };
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+            &Default::default()).unwrap();
         target.finish().unwrap();
 
 
