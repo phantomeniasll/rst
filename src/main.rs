50f94f4540b525a983afeac9dc902c634e8bcf01
Julian <julianmork@gmail.com>
2022-06-06 19:18:03 +0200

    Add perspective matrix and depth buffer

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
 
     let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
 
@@ -36,6 +36,15 @@ fn main() {
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
                                       &teapot::INDICES).unwrap();
 
+    let params = glium::DrawParameters {
+    depth: glium::Depth {
+        test: glium::draw_parameters::DepthTest::IfLess,
+        write: true,
+        .. Default::default()
+    },
+    .. Default::default()
+    };
+
 
     let vertex_shader_src = r#"
     #version 150
@@ -45,10 +54,11 @@ fn main() {
     out vec3 v_normal;
     out vec3 pos;
     uniform mat4 matrix;
+    uniform mat4 perspective;
 
     void main() {
         v_normal = transpose(inverse(mat3(matrix))) * normal;
-        gl_Position = matrix * vec4(position, 1.0);
+        gl_Position = perspective * matrix * vec4(position, 1.0);
         pos = gl_Position.xyz;
     }
 "#;
@@ -62,8 +72,8 @@ fn main() {
 
     void main() {
         float brightness = dot(normalize(v_normal), normalize(u_light));
-        vec4 dark_color = vec4(0.0, 0.2, cos(pos.y), 1.0);
-        vec4 bright_color = vec4(1.0, 1.0, sin(pos.x), 1.0);
+        vec4 dark_color = vec4(0.5, 0.1, 0.1, 1.0);
+        vec4 bright_color = vec4(1.,0.3, 0.3, 1.0);
         color = mix(dark_color, bright_color, brightness);
     }
 "#;
@@ -74,18 +84,37 @@ fn main() {
 
     event_loop.run(move |ev, _, control_flow| {
         let time = start.elapsed().as_secs_f32();
+
+
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        let perspective = {
+        let (width, height) = target.get_dimensions();
+        let aspect_ratio = height as f32 / width as f32;
+
+        let fov: f32 = 3.141592 / 3.0;
+        let zfar = 1024.0;
+        let znear = 0.1;
+
+        let f = 1.0 / (fov / 2.0).tan();
+
+        [
+            [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+            [         0.0         ,     f ,              0.0              ,   0.0],
+            [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+            [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+        ]
+        };
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
         let matrix = [
-            [0.01, 0.0, 0.0, 0.0],
+            [0.01, 0.0, 0.0,0.0],
             [0.0, 0.01, 0.0, 0.0],
             [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 0.0, 1.0f32]
+            [0.0, 0.0, 2.0, 1.0f32]
         ];
 
-        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, u_light: light },
-            &Default::default()).unwrap();
+        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, u_light: light, perspective: perspective },
+            &params).unwrap();
 
         target.finish().unwrap();
 
